29cc3875ca5a921199f07aa5d26bc5490d06574a
Devireddy Nitish Reddy <63440512+nitish666666@users.noreply.github.com>
2022-09-08 21:15:33 +0530

    Update views.py

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 # <HINT> Import any new Models here
-from .models import Course, Enrollment
+from .models import Course, Enrollment, Lesson, Question, Choice, Submission
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse
@@ -104,33 +104,63 @@ def enroll(request, course_id):
 
 
 # <HINT> Create a submit view to create an exam submission record for a course enrollment,
-# you may implement it based on following logic:
-         # Get user and course object, then get the associated enrollment object created when the user enrolled the course
-         # Create a submission object referring to the enrollment
-         # Collect the selected choices from exam form
-         # Add each selected choice object to the submission object
-         # Redirect to show_exam_result with the submission id
-#def submit(request, course_id):
+def submit(request, course_id):
+    user = request.user
+    course = get_object_or_404(Course, pk=course_id)
+    enrollment = Enrollment.objects.get(user=user, course=course)
+    submission = Submission.objects.create(enrollment=enrollment)
+
+    answers = extract_answers(request)
+    for answer in answers:
+        choice = get_object_or_404(Choice, pk=answer)
+        submission.choices.add(choice)
+        
+    return HttpResponseRedirect(reverse(viewname='onlinecourse:result', args=(course.id, submission.id)))
 
 
 # <HINT> A example method to collect the selected choices from the exam form from the request object
-#def extract_answers(request):
-#    submitted_anwsers = []
-#    for key in request.POST:
-#        if key.startswith('choice'):
-#            value = request.POST[key]
-#            choice_id = int(value)
-#            submitted_anwsers.append(choice_id)
-#    return submitted_anwsers
+def extract_answers(request):
+   submitted_answers = []
+   for key in request.POST:
+       if key.startswith('choice'):
+           value = request.POST[key]
+           choice_id = int(value)
+           submitted_answers.append(choice_id)
+   return submitted_answers
 
 
-# <HINT> Create an exam result view to check if learner passed exam and show their question results and result for each question,
-# you may implement it based on the following logic:
-        # Get course and submission based on their ids
-        # Get the selected choice ids from the submission record
-        # For each selected choice, check if it is a correct answer or not
-        # Calculate the total score
-#def show_exam_result(request, course_id, submission_id):
+# <HINT> Create an exam result view to check if learner passed exam and show their question results and result for each question
+def show_exam_result(request, course_id, submission_id):
+    course = get_object_or_404(Course, pk=course_id)
+    submission = get_object_or_404(Submission, pk=submission_id)
+    score = 0
+    correct_answers = 0
+    selected_ids = []
+
+    # Get the total number of correct answers
+    for question in course.question_set.all():
+        for choice in question.choice_set.all():
+            if choice.is_correct:
+                correct_answers += 1
+
+    # Get the score received by the user
+    for choice in submission.choices.all():
+        selected_ids.append(choice.id)
+        if choice.is_correct:
+            score += 1
+        else:
+            score -= 1
+
+    # Calculate the grade by finding the percentage of correct answers selected.
+    grade = int((score/correct_answers)*100)
+
+    # Context to go out to the exam result bootstrap
+    context = {}
+    context['course'] = course
+    context['selected_ids'] = selected_ids
+    context['grade'] = grade
+
+    return render(request, 'onlinecourse/exam_result_bootstrap.html', context)
 
 
 
